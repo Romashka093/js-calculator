84751028063110b646404ac2b5464a3dd3bd8274
Anna Romashenko <aa.romashchenko@gmail.com>
2020-06-01 18:06:56 +0300

    done

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,67 +1,28 @@
 'use strict';
 console.warn('Calculator');
 
-calculator.one.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.one.value));
+const ref = {
+	numberButtons: document.querySelectorAll('[data-number]'),
+	operatorButtons: document.querySelectorAll('[data-operator]'),
+	resetButton: document.querySelector('input[data-reset="reset"]')
+};
+
+ref.numberButtons.forEach((number) => {
+	number.addEventListener('click', (evt) => {
+		calculator.result.value += evt.target.value;
+	});
 });
 
-calculator.two.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.two.value));
-});
-
-calculator.three.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.three.value));
-});
-
-calculator.four.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.four.value));
-});
-
-calculator.five.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.five.value));
-});
-
-calculator.six.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.six.value));
-});
-
-calculator.seven.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.seven.value));
-});
-
-calculator.eight.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.eight.value));
-});
-
-calculator.nine.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.nine.value));
-});
-
-calculator.zero.addEventListener('click', () => {
-	console.log((calculator.result.value += calculator.zero.value));
+ref.operatorButtons.forEach((operator) => {
+	operator.addEventListener('click', (evt) => {
+		calculator.result.value += evt.target.value;
+	});
 });
 
 calculator.reset.addEventListener('click', () => {
-	console.log((calculator.result.value = ''));
-});
-
-calculator.divide.addEventListener('click', () => {
-	console.log((calculator.result.value += '/'));
-});
-
-calculator.multiply.addEventListener('click', () => {
-	console.log((calculator.result.value += '*'));
-});
-
-calculator.minus.addEventListener('click', () => {
-	console.log((calculator.result.value += '-'));
-});
-
-calculator.plus.addEventListener('click', () => {
-	console.log(typeof calculator.plus.value);
-	console.log((calculator.result.value += calculator.plus.value));
+	calculator.result.value = '';
 });
 
 calculator.equal.addEventListener('click', () => {
-	console.log((calculator.result.value = eval(calculator.result.value)));
+	calculator.result.value = eval(calculator.result.value);
 });
